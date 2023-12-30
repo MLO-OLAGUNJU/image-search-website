5c76b2bd0f90699a2d3dfd0f113da3b56cc629e6
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2023-12-30 01:48:01 +0100

    I created a serachImages fucntion

--- app.js
+++ app.js
@@ -4,7 +4,33 @@ const formElement = document.querySelector("form");
 const inputElement = document.querySelector("#search-input");
 const searchResults = document.querySelector(".search-results");
 const showMoreButton = document.querySelector("#show-more-button");
-const searchResult = document.querySelectorAll(".search-result");
 
 let inputData = "";
 let page = 1;
+
+async function searchImages() {
+  inputData = inputElement.value;
+  const url = `https://api.unsplash.com/search/photos?page${page}&query=${inputData}&client_id=${accessKey}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  const results = data.results;
+
+  if (page === 1) {
+    searchResults.textContent = "";
+  }
+
+  results.map((result) => {
+    const imageWrapper = document.createElement("div");
+    imageWrapper.classList.add("search-result");
+    const image = document.createElement("img");
+    image.src = result.urls.small;
+
+    image.alt = result.alt_description;
+    const imageLink = document.createElement("a");
+    imageLink.href = result.links.html;
+    imageLink.target = "_blank";
+    imageLink.textContent = result.alt_description;
+  });
+}
