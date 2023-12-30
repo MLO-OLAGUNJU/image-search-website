74b67ff879d3a7b8b6d5349137e6851c4c6dc3ed
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2023-12-30 01:32:16 +0100

    I created the needed variables and other declarations

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 const accessKey = "joCIb8vfNTFR6iA-dlM9roAw-J1T0vnmBAi0V_OWcBo";
+
+const formElement = document.querySelector("form");
+const inputElement = document.querySelector("#search-input");
+const searchResults = document.querySelector(".search-results");
+const showMoreButton = document.querySelector("#show-more-button");
+const searchResult = document.querySelectorAll(".search-result");
+
+let inputData = "";
+let page = 1;
